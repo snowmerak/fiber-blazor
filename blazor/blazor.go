267c781db7315cf45ae7b078c86bd19d5b90f20a
snowmerak <snowmerak@outlook.com>
2026-02-12 19:30:05 +0900

    feat(blazor): add rendering functions and HTML template for page structure

--- blazor/blazor.go
+++ blazor/blazor.go
@@ -3,7 +3,22 @@ package blazor
 import (
 	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/static"
 )
+
+const defaultTitle = "Fiber Blazor App"
+const defaultLang = "en"
+
+func InitRender(root templ.Component, lang string, title string) fiber.Handler {
+	return func(c fiber.Ctx) error {
+		c.Set(fiber.HeaderContentType, fiber.MIMETextHTMLCharsetUTF8)
+		return Page(title, lang, root).Render(c.Context(), c.Res().Response().BodyWriter())
+	}
+}
+
+func Static(app *fiber.App, prefix, rootDir string) {
+	app.Use(prefix, static.New(rootDir))
+}
 
 func SetRenderer[T, V any](componentFunc func(data *V) templ.Component, transform func(req *T) (*V, error)) fiber.Handler {
 	return func(c fiber.Ctx) error {
@@ -15,7 +30,9 @@ func SetRenderer[T, V any](componentFunc func(data *V) templ.Component, transfor
 		if err != nil {
 			return fiber.ErrBadRequest
 		}
+
 		component := componentFunc(data)
+
 		c.Set(fiber.HeaderContentType, fiber.MIMETextHTMLCharsetUTF8)
 		return component.Render(c.Context(), c.Res().Response().BodyWriter())
 	}
