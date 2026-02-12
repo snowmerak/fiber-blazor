337b16fb77d3a6b5be94516792ab6fb3254303d7
snowmerak <snowmerak@outlook.com>
2026-02-12 19:34:45 +0900

    feat(calculator): add simple addition calculator with HTMX integration

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/a-h/templ"
+	"github.com/gofiber/fiber/v3"
+	"github.com/snowmerak/fiber-blazor/blazor"
+)
+
+type CalcRequest struct {
+	A int `form:"a"`
+	B int `form:"b"`
+}
+
+type CalcData struct {
+	Sum int
+}
+
+func main() {
+	app := fiber.New()
+
+	// Statics 핸들러 등록
+	blazor.Static(app, "/statics", "./statics")
+
+	// Main page
+	app.Get("/", blazor.InitRender(Calculator(), "en", "Fiber Blazor Calculator"))
+
+	// HTMX calculation endpoint
+	app.Post("/calculate", blazor.SetRenderer(
+		func(data *CalcData) templ.Component {
+			return Result(data.Sum)
+		},
+		func(req *CalcRequest) (*CalcData, error) {
+			return &CalcData{Sum: req.A + req.B}, nil
+		},
+	))
+
+	log.Fatal(app.Listen(":3000"))
+}
