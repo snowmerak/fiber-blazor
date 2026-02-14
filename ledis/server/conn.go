d3c1ee722b2995c26a04bd1aa4eff88334fa6235
snowmerak <snowmerak@outlook.com>
2026-02-15 01:55:20 +0900

    feat: Implement server connection handling and Redis-like command execution for Ledis.

--- ledis/server/conn.go
+++ ledis/server/conn.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strconv"
 	"strings"
 
 	"github.com/snowmerak/fiber-blazor/ledis"
@@ -54,24 +55,26 @@ func (h *Handler) Handle(conn net.Conn) {
 
 		// Parse Command Name
 		cmdName := ""
-		if val.Array[0].Type == BulkString {
+		switch val.Array[0].Type {
+		case BulkString:
 			cmdName = val.Array[0].Bulk
-		} else if val.Array[0].Type == SimpleString {
+		case SimpleString:
 			cmdName = val.Array[0].Str
-		} else {
+		default:
 			h.writeError(conn, "ERR Invalid command format")
 			continue
 		}
 
 		args := make([]string, 0, len(val.Array)-1)
 		for i := 1; i < len(val.Array); i++ {
-			if val.Array[i].Type == BulkString {
+			switch val.Array[i].Type {
+			case BulkString:
 				args = append(args, val.Array[i].Bulk)
-			} else if val.Array[i].Type == SimpleString {
+			case SimpleString:
 				args = append(args, val.Array[i].Str)
-			} else if val.Array[i].Type == Integer {
+			case Integer:
 				args = append(args, fmt.Sprintf("%d", val.Array[i].Num))
-			} else {
+			default:
 				args = append(args, "") // Handle others?
 			}
 		}
@@ -88,22 +91,55 @@ func (h *Handler) execute(w io.Writer, cmd string, args []string) {
 	wr := NewWriter(w)
 
 	switch strings.ToUpper(cmd) {
+	// --- Generic ---
 	case "PING":
 		if len(args) > 0 {
 			wr.WriteBulkString(args[0])
 		} else {
 			wr.WriteSimpleString("PONG")
 		}
+	case "DEL":
+		if len(args) < 1 {
+			wr.WriteError("ERR wrong number of arguments for 'del' command")
+			return
+		}
+		count := 0
+		for _, key := range args {
+			if h.db.Exists(key) {
+				h.db.Del(key)
+				count++
+			}
+		}
+		wr.WriteInteger(int64(count))
+	case "EXISTS":
+		if len(args) < 1 {
+			wr.WriteError("ERR wrong number of arguments for 'exists' command")
+			return
+		}
+		count := 0
+		for _, key := range args {
+			if h.db.Exists(key) {
+				count++
+			}
+		}
+		wr.WriteInteger(int64(count))
+	case "TTL":
+		if len(args) != 1 {
+			wr.WriteError("ERR wrong number of arguments for 'ttl' command")
+			return
+		}
+		ttl := h.db.TTL(args[0])
+		wr.WriteInteger(int64(ttl.Seconds()))
+
+	// --- String ---
 	case "SET":
 		if len(args) < 2 {
 			wr.WriteError("ERR wrong number of arguments for 'set' command")
 			return
 		}
-		// TTL support? SET key val [EX seconds]
-		// For now simple SET
+		// TODO: Parse EX/PX options
 		h.db.Set(args[0], args[1], 0)
 		wr.WriteSimpleString("OK")
-
 	case "GET":
 		if len(args) != 1 {
 			wr.WriteError("ERR wrong number of arguments for 'get' command")
@@ -114,24 +150,421 @@ func (h *Handler) execute(w io.Writer, cmd string, args []string) {
 			wr.WriteNull()
 			return
 		}
-		// Value could be string or other types?
-		// ledis.Set takes interface{}.
-		// If we set string, we get string.
 		if s, ok := val.(string); ok {
 			wr.WriteBulkString(s)
 		} else {
-			// Convert to string
 			wr.WriteBulkString(fmt.Sprintf("%v", val))
 		}
+	case "MSET":
+		if len(args) == 0 || len(args)%2 != 0 {
+			wr.WriteError("ERR wrong number of arguments for 'mset' command")
+			return
+		}
+		pairs := make(map[string]interface{})
+		for i := 0; i < len(args); i += 2 {
+			pairs[args[i]] = args[i+1]
+		}
+		h.db.MSet(pairs)
+		wr.WriteSimpleString("OK")
+	case "MGET":
+		if len(args) == 0 {
+			wr.WriteError("ERR wrong number of arguments for 'mget' command")
+			return
+		}
+		vals := h.db.MGet(args...)
+		wr.WriteArray(len(vals))
+		for _, v := range vals {
+			if v == nil {
+				wr.WriteNull()
+			} else {
+				if s, ok := v.(string); ok {
+					wr.WriteBulkString(s)
+				} else {
+					wr.WriteBulkString(fmt.Sprintf("%v", v))
+				}
+			}
+		}
+	case "INCR":
+		if len(args) != 1 {
+			wr.WriteError("ERR wrong number of arguments for 'incr' command")
+			return
+		}
+		val, err := h.db.Incr(args[0])
+		if err != nil {
+			wr.WriteError(err.Error())
+		} else {
+			wr.WriteInteger(val)
+		}
+	case "DECR":
+		if len(args) != 1 {
+			wr.WriteError("ERR wrong number of arguments for 'decr' command")
+			return
+		}
+		val, err := h.db.Decr(args[0])
+		if err != nil {
+			wr.WriteError(err.Error())
+		} else {
+			wr.WriteInteger(val)
+		}
 
-	// ... Implement other commands ...
-	// Since this is a massive switch, maybe I should do just a few to prove it works
-	// as per "Integration Research" verification goal.
-	// User asked "RESP3를 구현하고 net.Conn만 재구현 하면 되겠네".
-	// The goal is to verify checking with go-redis.
-	// So implementation of PING, SET, GET is enough for verification.
+	// --- List ---
+	case "LPUSH":
+		if len(args) < 2 {
+			wr.WriteError("ERR wrong number of arguments for 'lpush' command")
+			return
+		}
+		count, err := h.db.LPush(args[0], stringToInterfaceSlice(args[1:])...)
+		if err != nil {
+			wr.WriteError(err.Error())
+		} else {
+			wr.WriteInteger(int64(count))
+		}
+	case "RPUSH":
+		if len(args) < 2 {
+			wr.WriteError("ERR wrong number of arguments for 'rpush' command")
+			return
+		}
+		count, err := h.db.RPush(args[0], stringToInterfaceSlice(args[1:])...)
+		if err != nil {
+			wr.WriteError(err.Error())
+		} else {
+			wr.WriteInteger(int64(count))
+		}
+	case "LPOP":
+		if len(args) != 1 {
+			wr.WriteError("ERR wrong number of arguments for 'lpop' command")
+			return
+		}
+		val, err := h.db.LPop(args[0])
+		if err != nil {
+			wr.WriteNull() // Assuming error means empty/missing
+		} else {
+			if s, ok := val.(string); ok {
+				wr.WriteBulkString(s)
+			} else {
+				wr.WriteBulkString(fmt.Sprintf("%v", val))
+			}
+		}
+	case "RPOP":
+		if len(args) != 1 {
+			wr.WriteError("ERR wrong number of arguments for 'rpop' command")
+			return
+		}
+		val, err := h.db.RPop(args[0])
+		if err != nil {
+			wr.WriteNull()
+		} else {
+			if s, ok := val.(string); ok {
+				wr.WriteBulkString(s)
+			} else {
+				wr.WriteBulkString(fmt.Sprintf("%v", val))
+			}
+		}
+	case "LLEN":
+		if len(args) != 1 {
+			wr.WriteError("ERR wrong number of arguments for 'llen' command")
+			return
+		}
+		l, err := h.db.LLen(args[0])
+		if err != nil {
+			wr.WriteInteger(0)
+		} else {
+			wr.WriteInteger(int64(l))
+		}
+	case "LRANGE":
+		if len(args) != 3 {
+			wr.WriteError("ERR wrong number of arguments for 'lrange' command")
+			return
+		}
+		start, err1 := strconv.ParseInt(args[1], 10, 64)
+		stop, err2 := strconv.ParseInt(args[2], 10, 64)
+		if err1 != nil || err2 != nil {
+			wr.WriteError("ERR value is not an integer or out of range")
+			return
+		}
+		vals, err := h.db.LRange(args[0], int(start), int(stop))
+		if err != nil {
+			wr.WriteArray(0)
+		} else {
+			wr.WriteArray(len(vals))
+			for _, v := range vals {
+				if s, ok := v.(string); ok {
+					wr.WriteBulkString(s)
+				} else {
+					wr.WriteBulkString(fmt.Sprintf("%v", v))
+				}
+			}
+		}
+
+	// --- Hash ---
+	case "HSET":
+		if len(args) < 3 || (len(args)-1)%2 != 0 {
+			wr.WriteError("ERR wrong number of arguments for 'hset' command")
+			return
+		}
+		// HSET key field value [field value ...]
+		count := 0
+		for i := 1; i < len(args); i += 2 {
+			n, err := h.db.HSet(args[0], args[i], args[i+1])
+			if err == nil {
+				count += int(n)
+			}
+		}
+		wr.WriteInteger(int64(count))
+	case "HGET":
+		if len(args) != 2 {
+			wr.WriteError("ERR wrong number of arguments for 'hget' command")
+			return
+		}
+		val, err := h.db.HGet(args[0], args[1])
+		if err != nil {
+			wr.WriteNull()
+		} else {
+			if s, ok := val.(string); ok {
+				wr.WriteBulkString(s)
+			} else {
+				wr.WriteBulkString(fmt.Sprintf("%v", val))
+			}
+		}
+	case "HDEL":
+		if len(args) < 2 {
+			wr.WriteError("ERR wrong number of arguments for 'hdel' command")
+			return
+		}
+		count := 0
+		for i := 1; i < len(args); i++ {
+			n, err := h.db.HDel(args[0], args[i])
+			if err == nil {
+				count += int(n)
+			}
+		}
+		wr.WriteInteger(int64(count))
+	case "HLEN":
+		if len(args) != 1 {
+			wr.WriteError("ERR wrong number of arguments for 'hlen' command")
+			return
+		}
+		l, err := h.db.HLen(args[0])
+		if err != nil {
+			wr.WriteInteger(0)
+		} else {
+			wr.WriteInteger(int64(l))
+		}
+	case "HGETALL":
+		if len(args) != 1 {
+			wr.WriteError("ERR wrong number of arguments for 'hgetall' command")
+			return
+		}
+		kv, err := h.db.HGetAll(args[0])
+		if err != nil {
+			wr.WriteArray(0)
+		} else {
+			wr.WriteArray(len(kv) * 2)
+			for k, v := range kv {
+				wr.WriteBulkString(k)
+				if s, ok := v.(string); ok {
+					wr.WriteBulkString(s)
+				} else {
+					wr.WriteBulkString(fmt.Sprintf("%v", v))
+				}
+			}
+		}
+
+	// --- Set ---
+	case "SADD":
+		if len(args) < 2 {
+			wr.WriteError("ERR wrong number of arguments for 'sadd' command")
+			return
+		}
+		count, err := h.db.SAdd(args[0], stringToInterfaceSlice(args[1:])...)
+		if err != nil {
+			wr.WriteError(err.Error())
+		} else {
+			wr.WriteInteger(int64(count))
+		}
+	case "SREM":
+		if len(args) < 2 {
+			wr.WriteError("ERR wrong number of arguments for 'srem' command")
+			return
+		}
+		count, err := h.db.SRem(args[0], stringToInterfaceSlice(args[1:])...)
+		if err != nil {
+			wr.WriteError(err.Error())
+		} else {
+			wr.WriteInteger(int64(count))
+		}
+	case "SMEMBERS":
+		if len(args) != 1 {
+			wr.WriteError("ERR wrong number of arguments for 'smembers' command")
+			return
+		}
+		members, err := h.db.SMembers(args[0])
+		if err != nil {
+			wr.WriteArray(0)
+		} else {
+			wr.WriteArray(len(members))
+			for _, m := range members {
+				if s, ok := m.(string); ok {
+					wr.WriteBulkString(s)
+				} else {
+					wr.WriteBulkString(fmt.Sprintf("%v", m))
+				}
+			}
+		}
+	case "SISMEMBER":
+		if len(args) != 2 {
+			wr.WriteError("ERR wrong number of arguments for 'sismember' command")
+			return
+		}
+		isMember, err := h.db.SIsMember(args[0], args[1])
+		if err != nil {
+			wr.WriteInteger(0)
+		} else {
+			if isMember {
+				wr.WriteInteger(1)
+			} else {
+				wr.WriteInteger(0)
+			}
+		}
+
+	// --- Sorted Set ---
+	case "ZADD":
+		if len(args) < 3 || (len(args)-1)%2 != 0 {
+			wr.WriteError("ERR wrong number of arguments for 'zadd' command")
+			return
+		}
+		// ZADD key score member [score member ...]
+		// Ledis ZAdd takes key, score, member.
+		// Loop
+		added := 0
+		for i := 1; i < len(args); i += 2 {
+			score, err := strconv.ParseFloat(args[i], 64)
+			if err != nil {
+				wr.WriteError("ERR value is not a valid float")
+				return
+			}
+			member := args[i+1]
+			count, err := h.db.ZAdd(args[0], score, member)
+			if err == nil {
+				added += count
+			}
+		}
+		wr.WriteInteger(int64(added))
+	case "ZRANGE":
+		if len(args) != 3 {
+			wr.WriteError("ERR wrong number of arguments for 'zrange' command")
+			return
+		}
+		start, err1 := strconv.ParseInt(args[1], 10, 64)
+		stop, err2 := strconv.ParseInt(args[2], 10, 64)
+		if err1 != nil || err2 != nil {
+			wr.WriteError("ERR value is not an integer")
+			return
+		}
+		// ZRange(key, start, stop, withScores)
+		res, err := h.db.ZRange(args[0], start, stop, false)
+		if err != nil {
+			wr.WriteArray(0)
+		} else {
+			wr.WriteArray(len(res))
+			for _, item := range res {
+				if s, ok := item.(string); ok {
+					wr.WriteBulkString(s)
+				} else if f, ok := item.(float64); ok {
+					wr.WriteBulkString(fmt.Sprintf("%g", f)) // Use %g for float
+				} else {
+					wr.WriteBulkString(fmt.Sprintf("%v", item))
+				}
+			}
+		}
+
+	// --- Bitmap ---
+	case "SETBIT":
+		if len(args) != 3 {
+			wr.WriteError("ERR wrong number of arguments for 'setbit' command")
+			return
+		}
+		offset, err1 := strconv.ParseUint(args[1], 10, 64)
+		val, err2 := strconv.ParseInt(args[2], 10, 64)
+		if err1 != nil || err2 != nil {
+			wr.WriteError("ERR bit or offset is not an integer")
+			return
+		}
+		oldVal, err := h.db.SetBit(args[0], offset, int(val))
+		if err != nil {
+			wr.WriteError(err.Error())
+		} else {
+			wr.WriteInteger(int64(oldVal))
+		}
+	case "GETBIT":
+		if len(args) != 2 {
+			wr.WriteError("ERR wrong number of arguments for 'getbit' command")
+			return
+		}
+		offset, err := strconv.ParseUint(args[1], 10, 64)
+		if err != nil {
+			wr.WriteError("ERR offset is not an integer")
+			return
+		}
+		val, err := h.db.GetBit(args[0], offset)
+		if err != nil {
+			wr.WriteInteger(0)
+		} else {
+			wr.WriteInteger(int64(val))
+		}
+	case "BITCOUNT":
+		if len(args) != 1 {
+			// Ignoring start/end arguments for now as our BitCount doesn't support them easily?
+			// Checking ledis_bitmap.go... BitCount(key string) (uint64, error)
+			// So strict 1 arg.
+			if len(args) > 1 {
+				wr.WriteError("ERR syntax error (arguments not supported for bitcount yet)")
+				return
+			}
+			wr.WriteError("ERR wrong number of arguments for 'bitcount' command")
+			return
+		}
+		count, err := h.db.BitCount(args[0])
+		if err != nil {
+			wr.WriteInteger(0)
+		} else {
+			wr.WriteInteger(int64(count))
+		}
+
+	// --- Pub/Sub ---
+	case "PUBLISH":
+		if len(args) != 2 {
+			wr.WriteError("ERR wrong number of arguments for 'publish' command")
+			return
+		}
+		// Publish returns int64, no error
+		count := h.db.Publish(args[0], args[1])
+		wr.WriteInteger(count)
+
+	// --- Stream ---
+	case "XADD":
+		if len(args) < 2 {
+			wr.WriteError("ERR wrong number of arguments for 'xadd' command")
+			return
+		}
+		// XADD key ID field value ...
+		id, err := h.db.XAdd(args[0], args[1], args[2:]...)
+		if err != nil {
+			wr.WriteError(err.Error())
+		} else {
+			// Redis XADD returns the ID as bulk string usually.
+			wr.WriteBulkString(id)
+		}
 
 	default:
 		wr.WriteError(fmt.Sprintf("ERR unknown command '%s'", cmd))
 	}
+}
+
+func stringToInterfaceSlice(args []string) []interface{} {
+	iface := make([]interface{}, len(args))
+	for i, v := range args {
+		iface[i] = v
+	}
+	return iface
 }
---
+++ ledis/server/integration_test.go
@@ -0,0 +1,211 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"sort"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/snowmerak/fiber-blazor/ledis"
+)
+
+func TestComprehensiveIntegration(t *testing.T) {
+	db := ledis.New(16)
+	rdb := NewGoRedisClient(db)
+	defer rdb.Close()
+	ctx := context.Background()
+
+	t.Run("Strings", func(t *testing.T) {
+		// SET/GET
+		if err := rdb.Set(ctx, "skey", "sval", 0).Err(); err != nil {
+			t.Fatal(err)
+		}
+		if val, err := rdb.Get(ctx, "skey").Result(); err != nil || val != "sval" {
+			t.Fatalf("GET failed: %v, %s", err, val)
+		}
+		// INCR / DECR
+		rdb.Set(ctx, "ctr", "10", 0)
+		if val, err := rdb.Incr(ctx, "ctr").Result(); err != nil || val != 11 {
+			t.Fatalf("INCR failed: %v, %d", err, val)
+		}
+		if val, err := rdb.Decr(ctx, "ctr").Result(); err != nil || val != 10 {
+			t.Fatalf("DECR failed: %v, %d", err, val)
+		}
+		// MSET / MGET
+		if err := rdb.MSet(ctx, "k1", "v1", "k2", "v2").Err(); err != nil {
+			t.Fatal(err)
+		}
+		vals, err := rdb.MGet(ctx, "k1", "k2", "missing").Result()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(vals) != 3 {
+			t.Fatalf("MGET len mismatch")
+		}
+		if vals[0] != "v1" || vals[1] != "v2" || vals[2] != nil {
+			t.Fatalf("MGET values mismatch: %v", vals)
+		}
+	})
+
+	t.Run("Lists", func(t *testing.T) {
+		rdb.Del(ctx, "mylist")
+		// LPUSH
+		if err := rdb.LPush(ctx, "mylist", "world", "hello").Err(); err != nil {
+			t.Fatal(err)
+		}
+		// LRANGE
+		vals, err := rdb.LRange(ctx, "mylist", 0, -1).Result()
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := []string{"hello", "world"}
+		if !reflect.DeepEqual(vals, expected) {
+			t.Fatalf("LRANGE mismatch: got %v, want %v", vals, expected)
+		}
+		// LLEN
+		if l, err := rdb.LLen(ctx, "mylist").Result(); err != nil || l != 2 {
+			t.Fatalf("LLEN failed: %v, %d", err, l)
+		}
+		// RPOP
+		if val, err := rdb.RPop(ctx, "mylist").Result(); err != nil || val != "world" {
+			t.Fatalf("RPOP failed: %v, %s", err, val)
+		}
+	})
+
+	t.Run("Hashes", func(t *testing.T) {
+		rdb.Del(ctx, "myhash")
+		// HSET
+		if err := rdb.HSet(ctx, "myhash", "f1", "v1", "f2", "v2").Err(); err != nil {
+			t.Fatal(err)
+		}
+		// HGET
+		if val, err := rdb.HGet(ctx, "myhash", "f1").Result(); err != nil || val != "v1" {
+			t.Fatalf("HGET failed: %v, %s", err, val)
+		}
+		// HGETALL
+		all, err := rdb.HGetAll(ctx, "myhash").Result()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(all) != 2 || all["f1"] != "v1" || all["f2"] != "v2" {
+			t.Fatalf("HGETALL mismatch: %v", all)
+		}
+		// HLEN
+		if l, err := rdb.HLen(ctx, "myhash").Result(); err != nil || l != 2 {
+			t.Fatalf("HLEN failed: %v, %d", err, l)
+		}
+	})
+
+	t.Run("Sets", func(t *testing.T) {
+		rdb.Del(ctx, "myset")
+		// SADD
+		if err := rdb.SAdd(ctx, "myset", "a", "b", "c").Err(); err != nil {
+			t.Fatal(err)
+		}
+		// SISMEMBER
+		if is, err := rdb.SIsMember(ctx, "myset", "a").Result(); err != nil || !is {
+			t.Fatalf("SISMEMBER a failed")
+		}
+		if is, err := rdb.SIsMember(ctx, "myset", "z").Result(); err != nil || is {
+			t.Fatalf("SISMEMBER z failed")
+		}
+		// SMEMBERS
+		members, err := rdb.SMembers(ctx, "myset").Result()
+		if err != nil {
+			t.Fatal(err)
+		}
+		sort.Strings(members)
+		expected := []string{"a", "b", "c"}
+		if !reflect.DeepEqual(members, expected) {
+			t.Fatalf("SMEMBERS mismatch: got %v, want %v", members, expected)
+		}
+	})
+
+	t.Run("SortedSets", func(t *testing.T) {
+		rdb.Del(ctx, "myzset")
+		// ZADD
+		members := []redis.Z{
+			{Score: 1.0, Member: "one"},
+			{Score: 2.0, Member: "two"},
+			{Score: 3.0, Member: "three"},
+		}
+		if err := rdb.ZAdd(ctx, "myzset", members...).Err(); err != nil {
+			t.Fatal(err)
+		}
+		// ZRANGE
+		vals, err := rdb.ZRange(ctx, "myzset", 0, -1).Result()
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := []string{"one", "two", "three"}
+		if !reflect.DeepEqual(vals, expected) {
+			t.Fatalf("ZRANGE mismatch: got %v, want %v", vals, expected)
+		}
+	})
+
+	t.Run("Bitmaps", func(t *testing.T) {
+		rdb.Del(ctx, "mybit")
+		// SETBIT
+		if err := rdb.SetBit(ctx, "mybit", 10, 1).Err(); err != nil {
+			t.Fatal(err)
+		}
+		// GETBIT
+		if val, err := rdb.GetBit(ctx, "mybit", 10).Result(); err != nil || val != 1 {
+			t.Fatalf("GETBIT 10 failed")
+		}
+		if val, err := rdb.GetBit(ctx, "mybit", 0).Result(); err != nil || val != 0 {
+			t.Fatalf("GETBIT 0 failed")
+		}
+		// BITCOUNT
+		if count, err := rdb.BitCount(ctx, "mybit", nil).Result(); err != nil || count != 1 {
+			t.Fatalf("BITCOUNT failed: %v, %d", err, count)
+		}
+	})
+
+	t.Run("PubSub", func(t *testing.T) {
+		// Just testing PUBLISH for now as SUBSCRIBE blocks
+		// PUBLISH
+		if count, err := rdb.Publish(ctx, "chan1", "msg").Result(); err != nil {
+			t.Fatal(err)
+		} else if count != 0 {
+			// No subscribers yet
+			t.Fatalf("Expected 0 subscribers, got %d", count)
+		}
+		// Real pubsub test would need a separate goroutine consuming msgChan
+	})
+
+	t.Run("Streams", func(t *testing.T) {
+		rdb.Del(ctx, "mystream")
+		// XADD
+		// go-redis XAddArgs
+		id, err := rdb.XAdd(ctx, &redis.XAddArgs{
+			Stream: "mystream",
+			Values: map[string]interface{}{"f1": "v1"},
+		}).Result()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if id == "" {
+			t.Fatal("XADD returned empty ID")
+		}
+		fmt.Println("XADD ID:", id)
+	})
+
+	// Complex case: concurrent operations via map
+	t.Run("Concurrent", func(t *testing.T) {
+		done := make(chan bool)
+		for i := 0; i < 10; i++ {
+			go func(id int) {
+				key := fmt.Sprintf("ckey:%d", id)
+				rdb.Set(ctx, key, id, 0)
+				rdb.Get(ctx, key)
+				done <- true
+			}(i)
+		}
+		for i := 0; i < 10; i++ {
+			<-done
+		}
+	})
+}
